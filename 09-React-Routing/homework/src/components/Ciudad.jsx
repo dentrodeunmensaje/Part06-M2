9afb0a693132e8b96e2bf57e31d7f06e36771d21
dentrodeunmensaje <dentrodeunmensaje@hotmail.com>
2022-05-12 19:22:52 -0600

    finished routing homework final detail

--- 09-React-Routing/homework/src/components/Ciudad.jsx
+++ 09-React-Routing/homework/src/components/Ciudad.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 
-export default function Ciudad({city}) {
-    return (
-        <div className="ciudad">
-                <div className="container">
-                    <h2>{city.name}</h2>
-                    <div className="info">
-                        <div>Temperatura: {city.temp} ºC</div>
-                        <div>Clima: {city.weather}</div>
-                        <div>Viento: {city.wind} km/h</div>
-                        <div>Cantidad de nubes: {city.clouds}</div>
-                        <div>Latitud: {city.latitud}º</div>
-                        <div>Longitud: {city.longitud}º</div>
-                    </div>
-            </div>
+export default function Ciudad({ city }) {
+  if (!city) {
+    alert("la ciudad no existe");
+    return (<div>La ciudad no existe</div>);
+  }
+  return (
+    <div className="ciudad">
+      <div className="container">
+        <h2>{city.name}</h2>
+        <div className="info">
+          <div>Temperatura: {city.temp} ºC</div>
+          <div>Clima: {city.weather}</div>
+          <div>Viento: {city.wind} km/h</div>
+          <div>Cantidad de nubes: {city.clouds}</div>
+          <div>Latitud: {city.latitud}º</div>
+          <div>Longitud: {city.longitud}º</div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
